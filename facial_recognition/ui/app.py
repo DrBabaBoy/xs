d60ce46542ce714e491e290b538eee8cd723284d
DrBabaBoy <bababoyxyz@hotmail.com>
2024-05-11 23:18:30 -0600

    ajuste registro y inicio

--- facial_recognition/ui/app.py
+++ facial_recognition/ui/app.py
@@ -205,7 +205,7 @@ def app(page: ft.Page) -> None:
     ))
 
     page.appbar = custom_app_bar()
-    page.theme = ft.Theme(color_scheme_seed=ft.colors.YELLOW)
+    page.theme = ft.Theme(color_scheme_seed=ft.colors.PURPLE)
     page.update()
     page.window_center()
 
